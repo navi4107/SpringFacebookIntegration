b2a62d8b5b06dc3928f40e744eb2a47e2a8c56aa
Avinash Narisetty <JosephAvinash.Narisetty@centurylink.com>
2019-07-04 12:35:44 +0530

    facebook integration initial commit

---
+++ src/main/java/com/example/SpringFacebookIntegration/SpringFacebookIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringFacebookIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacebookIntegrationApplication.class, args);
+	}
+
+	//
+}
---
+++ src/test/java/com/example/SpringFacebookIntegration/SpringFacebookIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SpringFacebookIntegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFacebookIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
